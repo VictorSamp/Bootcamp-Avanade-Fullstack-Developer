71d1b3289b66f6bfa6e5749a254da0c2fb3f19c3
VictorSamp <victor.msampaio@outlook.com.br>
2021-01-19 09:45:02 -0300

    Finalizando aula 'Fazendo requisições em HTTP'

--- Projetos/site-de-cursos/src/app/courses/course-info.component.ts
+++ Projetos/site-de-cursos/src/app/courses/course-info.component.ts
@@ -9,16 +9,23 @@ import { CourseService } from './course.service';
 
 export class CourseInfoComponent implements OnInit {
 
-    course!: Course;
+    course: Course;
 
     constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService){ }
 
     ngOnInit(): void {
-        this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')!);
+        this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')!).subscribe({
+            next: course => this.course = course,
+            error: err => console.log('Error', err)
+        });
+        
     }
 
     save(): void {
-        this.courseService.save(this.course);
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log('Saved witch success', course),
+            error: err => console.log('Error', err)
+        });
     }
 
 }
